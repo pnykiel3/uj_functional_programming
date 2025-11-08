e8c79955bbf72c47d318f2ca2917c6c37ab97c18
pawelnykiel <p.nykiel@icloud.com>
2025-11-08 16:48:23 +0100

    main updated

--- assignment3/src/main/scala/Main.scala
+++ assignment3/src/main/scala/Main.scala
@@ -1,16 +1,11 @@
-import cask.model.Response
-
-
 object Main extends cask.MainRoutes {
 
   override def host: String = "0.0.0.0"
+
   override def port: Int = 8080
 
   private def tail[A](list: List[A]): List[A] = {
-    list match {
-      case Nil => throw new NoSuchElementException("Your list is empty!")
-      case _ :: tail => tail
-    }
+    list.tail
   }
 
   def drop[A](list: DoubleLinkedList[A], n: Int): DoubleLinkedList[A] = {
@@ -25,36 +20,72 @@ object Main extends cask.MainRoutes {
     list
   }
 
-  @cask.post("/tail")
-  def tailEndpoint(request: cask.Request): Response[ujson.Value] = {
-    try {
-      val data = ujson.read(request.text())
-      val list = data("list").arr.toList.map(_.num.toInt)
-      val result = tail(list)
-      cask.Response(ujson.Obj("tail result: " -> result), statusCode = 200)
-    } catch {
-      case e: Exception => cask.Response(ujson.Obj("error" -> e.getMessage), statusCode = 400)
+  def dropWhile[A](list: DoubleLinkedList[A], predicate: A => Boolean): DoubleLinkedList[A] = {
+    def dropWhileSupport(remaining: DoubleLinkedList[A]): DoubleLinkedList[A] = {
+      val remainingList = remaining.toList
+      remainingList match {
+        case Nil => remaining
+        case _ if predicate(remainingList.last) =>
+          remaining.pop_back()
+          dropWhileSupport(remaining)
+        case _ => remaining
+      }
+    }
+
+    dropWhileSupport(list)
+  }
+
+  @cask.postJson("/tail")
+  def tailEndpoint(list: List[Int]): cask.Response[ujson.Value] = {
+
+    if (list.isEmpty) {
+      cask.Response(
+        ujson.Obj("error" -> "Cannot get tail of an empty list"),
+        statusCode = 400
+      )
+    } else {
+      val tail = list.tail
+      val jsonList = tail.map(x => ujson.Num(x.toDouble))
+      cask.Response(ujson.Obj("tail result" -> jsonList), statusCode = 200)
     }
   }
 
-  @cask.post("/drop")
-  def dropEndpoint(request: cask.Request): Response[ujson.Value] = {
-    try {
-      val data = ujson.read(request.text())
-      val listData = data("list").arr.toList.map(_.num.toInt)
-      val n = data("n").num.toInt
-      val list = new DoubleLinkedList[Int](listData)
-      drop(list, n)
-
-      val resultList = list.toList
+  @cask.postJson("/drop")
+  def dropEndpoint(list: List[Int], n: Int): cask.Response[ujson.Value] = {
+    if (n > list.length) {
       cask.Response(
-      ujson.Obj("drop result" -> ujson.Arr(resultList.map(x => ujson.Num(x.toDouble)): _*)),
-      statusCode = 200)
+        ujson.Obj("error" -> "n is larger than the list size"),
+        statusCode = 400
+      )
+    } else if (n < 0) {
+      cask.Response(
+        ujson.Obj("error" -> "n must be positive"),
+        statusCode = 400
+      )
+    } else {
+    val dll = new DoubleLinkedList[Int](list)
+    drop(dll, n)
+    val resultJson = ujson.Arr(dll.toList.map(x => ujson.Num(x.toDouble)): _*)
+    cask.Response(ujson.Obj("drop result" -> resultJson), statusCode = 200)
+      }
+  }
+
+  @cask.postJson("/dropWhile")
+  def dropWhileEndpoint(list: List[Int], predicate: String): ujson.Value = {
+    val pred: Int => Boolean = predicate match {
+      case "even" => (x: Int) => x % 2 == 0
+      case "odd" => (x: Int) => x % 2 != 0
+      case "positive" => (x: Int) => x > 0
+      case "negative" => (x: Int) => x < 0
+      case s"div_$y" => (x: Int) => x % y.toInt == 0
+      case s"<$y" => (x: Int) => x < y.toInt
+      case s">$y" => (x:Int) => x > y.toInt
+      case _ => throw new IllegalArgumentException("Unsupported predicate type")
     }
-    catch {
-    case e: Exception => 
-      cask.Response(ujson.Obj("error" -> e.getMessage), statusCode = 400)
-    }
+
+    val dll = new DoubleLinkedList[Int](list)
+    dropWhile(dll, pred)
+    ujson.Obj("dropWhile result" -> ujson.Arr(dll.toList.map(x => ujson.Num(x.toDouble)): _*))
   }
 
   @cask.get("/")
